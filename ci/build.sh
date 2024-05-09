5a991a92df4e7c21a3285fdb14de345e5c945ccf
odrling <florianbadie@odrling.xyz>
2024-05-09 12:03:47 +0200

    ci: don't install go in build script
    build image now includes go

--- ci/build.sh
+++ ci/build.sh
@@ -2,10 +2,6 @@
 [ -n "$CHOST" ] && crossarg=--cross-file ci/$CHOST.txt
 set -e
 
-apk add chimera-repo-contrib
-apk add go
-
-
 meson setup /build --buildtype release --strip -Db_lto=true -Db_lto_mode=thin -Dffmpegaacsucks:default_library=static -Ddakara_check:default_library=static -Db_pie=true -Dffmpeg:programs=disabled -Dffmpeg:tests=disabled -Dffmpeg:encoders=disabled -Dffmpeg:muxers=disabled -Dffmpeg:avfilter=disabled -Dffmpeg:avdevice=disabled -Dffmpeg:postproc=disabled -Dffmpeg:swresample=disabled -Dffmpeg:swscale=disabled -Dffmpeg:decoders=disabled -Dffmpeg:aac_decoder=enabled -Dffmpeg:aac_fixed_decoder=enabled -Dffmpeg:aac_latm_decoder=enabled -Dffmpeg:version3=enabled $crossarg
 meson compile -C /build
 meson install -C /build --destdir /image --skip-subprojects
