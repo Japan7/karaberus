375f8eed6def86fe6cb3eebf93dcfcd51d93a842
odrling <florianbadie@odrling.xyz>
2024-05-28 05:32:48 +0200

    remove test db before tests
    so we're sure we are not running the tests on top of previous runs as
    the tests don't expect that

--- server/db.go
+++ server/db.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/glebarez/sqlite"
+	"github.com/gofiber/fiber/v2/log"
 	"gorm.io/gorm"
 )
 
@@ -11,6 +13,15 @@ var db_instance *gorm.DB = nil
 
 func init_db() {
 	db_file := getEnvDefault("DB_FILE", "karaberus.db")
+	db_test := getEnvDefault("TEST", "")
+
+	if db_test != "" {
+		err := os.Remove(db_file)
+		// probably errors don't matter
+		if err != nil {
+			log.Warn(err.Error())
+		}
+	}
 
 	fmt.Printf("DB file: %s", db_file)
 	db, err := gorm.Open(sqlite.Open(db_file), &gorm.Config{})
