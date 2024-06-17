4baff9e6512bec110dcda351c7640058bc532d3f
odrling <florianbadie@odrling.xyz>
2024-06-17 13:02:20 +0200

    meson: only install ui deps once

--- scripts/npm_run.py
+++ scripts/npm_run.py
@@ -1,5 +1,6 @@
-import subprocess
 import os
+import pathlib
+import subprocess
 import sys
 
 def run(*args: str):
@@ -10,12 +11,16 @@ def main():
     ui_source = os.environ["SOURCE"]
 
     npm_cmd = npm, "-C", ui_source
-    run(*npm_cmd, "ci")
 
     npm_subcmd = sys.argv[1]
     if npm_subcmd == "build":
         outdir = sys.argv[2]
         run(*npm_cmd, "run", npm_subcmd, "--", "--outDir", outdir)
+    elif npm_subcmd == "ci":
+        run(*npm_cmd, "ci")
+        outpath = sys.argv[2]
+        with open(outpath, "w"):
+            pass
     else:
         run(*npm_cmd, "run", npm_subcmd)
 
