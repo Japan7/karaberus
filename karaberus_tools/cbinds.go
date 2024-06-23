0f1df2228dff3ca7a2bb4a9d333603796831c044
odrling <florianbadie@odrling.xyz>
2024-06-23 18:35:08 +0200

    move cgo bindings to karaberus_tools package
    so the server package should be free of errors even when pkg-config
    can't find karaberus_tools.

---
+++ karaberus_tools/cbinds.go
@@ -0,0 +1,60 @@
+package karaberus_tools
+
+/*
+#cgo pkg-config: karaberus_tools
+#include <karaberus_tools.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <string.h>
+
+int AVIORead(void *obj, uint8_t *buf, int n);
+int64_t AVIOSeek(void *obj, int64_t offset, int whence);
+
+static inline struct dakara_check_results *karaberus_dakara_check(void *obj) {
+  return karaberus_dakara_check_avio(obj, AVIORead, AVIOSeek);
+}
+*/
+import "C"
+import (
+	"errors"
+	"io"
+	"unsafe"
+
+	"github.com/mattn/go-pointer"
+	"github.com/minio/minio-go/v7"
+)
+
+//export AVIORead
+func AVIORead(opaque unsafe.Pointer, buf *C.uint8_t, n C.int) C.int {
+	obj := pointer.Restore(opaque).(*minio.Object)
+	rbuf := make([]byte, n)
+	nread, err := obj.Read(rbuf)
+	if err != nil && !errors.Is(err, io.EOF) {
+		panic(err)
+	}
+	C.memcpy(C.CBytes(rbuf), unsafe.Pointer(buf), C.size_t(nread))
+	return C.int(nread)
+}
+
+//export AVIOSeek
+func AVIOSeek(opaque unsafe.Pointer, offset C.int64_t, whence C.int) C.int64_t {
+	obj := pointer.Restore(opaque).(*minio.Object)
+	pos, err := obj.Seek(int64(offset), int(whence))
+	if err != nil {
+		panic(err)
+	}
+	return C.int64_t(pos)
+}
+
+type DakaraCheckResultsOutput struct {
+	Passed bool `json:"passed" example:"true" doc:"true if file passed all checks"`
+}
+
+func DakaraCheckResults(obj *minio.Object) DakaraCheckResultsOutput {
+	res := C.karaberus_dakara_check(pointer.Save(obj))
+	defer C.karaberus_dakara_check_results_free(res)
+
+	out := DakaraCheckResultsOutput{Passed: bool(res.passed)}
+	return out
+}
--- server/s3.go
+++ server/s3.go
@@ -3,31 +3,13 @@
 
 package server
 
-/*
-#cgo pkg-config: karaberus_tools
-#include <karaberus_tools.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdint.h>
-#include <string.h>
-
-int AVIORead(void *obj, uint8_t *buf, int n);
-int64_t AVIOSeek(void *obj, int64_t offset, int whence);
-
-static inline struct dakara_check_results *karaberus_dakara_check(void *obj) {
-  return karaberus_dakara_check_avio(obj, AVIORead, AVIOSeek);
-}
-*/
-import "C"
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"path/filepath"
-	"unsafe"
 
-	pointer "github.com/mattn/go-pointer"
+	"github.com/Japan7/karaberus/karaberus_tools"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
@@ -75,9 +57,9 @@ type CheckS3FileOutput struct {
 }
 
 type CheckKaraOutput struct {
-	Video        *CheckS3FileOutput
-	Instrumental *CheckS3FileOutput
-	Subtitles    *CheckS3FileOutput
+	Video        *karaberus_tools.DakaraCheckResultsOutput
+	Instrumental *karaberus_tools.DakaraCheckResultsOutput
+	Subtitles    *karaberus_tools.DakaraCheckResultsOutput
 }
 
 func CheckKara(ctx context.Context, kara KaraInfoDB) (*CheckKaraOutput, error) {
@@ -111,29 +93,7 @@ func CheckKara(ctx context.Context, kara KaraInfoDB) (*CheckKaraOutput, error) {
 	return out, nil
 }
 
-//export AVIORead
-func AVIORead(opaque unsafe.Pointer, buf *C.uint8_t, n C.int) C.int {
-	obj := pointer.Restore(opaque).(*minio.Object)
-	rbuf := make([]byte, n)
-	nread, err := obj.Read(rbuf)
-	if err != nil && !errors.Is(err, io.EOF) {
-		panic(err)
-	}
-	C.memcpy(C.CBytes(rbuf), unsafe.Pointer(buf), C.size_t(nread))
-	return C.int(nread)
-}
-
-//export AVIOSeek
-func AVIOSeek(opaque unsafe.Pointer, offset C.int64_t, whence C.int) C.int64_t {
-	obj := pointer.Restore(opaque).(*minio.Object)
-	pos, err := obj.Seek(int64(offset), int(whence))
-	if err != nil {
-		panic(err)
-	}
-	return C.int64_t(pos)
-}
-
-func CheckS3File(ctx context.Context, video_filename string) (*CheckS3FileOutput, error) {
+func CheckS3File(ctx context.Context, video_filename string) (*karaberus_tools.DakaraCheckResultsOutput, error) {
 	client := getS3Client()
 
 	obj, err := client.GetObject(ctx, CONFIG.S3.BucketName, video_filename, minio.GetObjectOptions{})
@@ -141,12 +101,7 @@ func CheckS3File(ctx context.Context, video_filename string) (*CheckS3FileOutput
 		return nil, err
 	}
 
-	res := C.karaberus_dakara_check(pointer.Save(obj))
-	defer C.karaberus_dakara_check_results_free(res)
+	res := karaberus_tools.DakaraCheckResults(obj)
 
-	out := &CheckS3FileOutput{
-		Passed: bool(res.passed),
-	}
-
-	return out, nil
+	return &res, nil
 }
