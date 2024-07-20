0d34aed9be8c9f3cc67e22326fff72c8c329c64c
odrling <florianbadie@odrling.xyz>
2024-07-20 13:50:49 +0200

    ci: fix cross compilation
    shell moment

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-[ -n "$CHOST" ] && crossarg=--cross-file ci/$CHOST.txt
+[ -n "$CHOST" ] && crossarg="--cross-file ci/$CHOST.txt"
 set -e
 
 meson setup /build --buildtype release --strip -Db_lto=true -Db_lto_mode=thin -Dffmpegaacsucks:default_library=static -Ddakara_check:default_library=static -Db_pie=true -Dffmpeg:programs=disabled -Dffmpeg:tests=disabled -Dffmpeg:encoders=disabled -Dffmpeg:muxers=disabled -Dffmpeg:avfilter=disabled -Dffmpeg:avdevice=disabled -Dffmpeg:postproc=disabled -Dffmpeg:swresample=disabled -Dffmpeg:swscale=disabled -Dffmpeg:decoders=disabled -Dffmpeg:aac_decoder=enabled -Dffmpeg:aac_fixed_decoder=enabled -Dffmpeg:aac_latm_decoder=enabled -Dffmpeg:version3=enabled $crossarg
