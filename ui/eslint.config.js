d7b8fb3b9fe3450af633d3e5882533c3927efeea
NextFire <git@yuru.moe>
2024-08-02 13:55:38 +0200

    ui: eslint 9 + tseslint 8 + flatconfig

---
+++ ui/eslint.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  // @ts-expect-error: prettier types are not up-to-date
+  eslintPluginPrettierRecommended,
+  {
+    ignores: ["dist", "src/utils/karaberus.d.ts"],
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { varsIgnorePattern: "^_", args: "none" },
+      ],
+    },
+  },
+);
