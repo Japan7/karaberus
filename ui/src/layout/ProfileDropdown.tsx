f280ae6e971d233e94dec8fb0b5f452902cb0a70
NextFire <git@yuru.moe>
2024-08-04 21:50:36 +0200

    ui: fix dropdown close

--- ui/src/layout/ProfileDropdown.tsx
+++ ui/src/layout/ProfileDropdown.tsx
@@ -6,34 +6,42 @@ import {
 } from "../utils/session";
 
 export default function ProfileDropdown(props: { infos: KaraberusJwtPayload }) {
-  let detailsRef!: HTMLDetailsElement;
-
   const logout: JSX.EventHandler<HTMLElement, MouseEvent> = () => {
     clearSession();
     location.href = "/";
   };
 
+  const closeDropdown = () => {
+    const elem = document.activeElement;
+    if (elem instanceof HTMLElement) {
+      elem.blur();
+    }
+  };
+
   return (
-    <details ref={detailsRef} class="dropdown dropdown-bottom dropdown-end">
-      <summary class="btn btn-ghost btn-circle avatar">
+    <div class="dropdown dropdown-bottom dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
           <img alt={props.infos.name} src={props.infos.picture} />
         </div>
-      </summary>
-      <ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li onclick={() => detailsRef.removeAttribute("open")}>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+      >
+        <li onclick={() => closeDropdown()}>
           <a href="/profile" class="flex flex-col gap-0">
             <span class="w-full font-bold text-lg">{props.infos.name}</span>
             <span class="w-full font-bold">{isAdmin() ? "Admin" : "User"}</span>
           </a>
         </li>
-        <li onclick={() => detailsRef.removeAttribute("open")}>
+        <li onclick={() => closeDropdown()}>
           <a href="/settings">Settings</a>
         </li>
-        <li onclick={() => detailsRef.removeAttribute("open")}>
+        <li onclick={() => closeDropdown()}>
           <a onclick={logout}>Logout</a>
         </li>
       </ul>
-    </details>
+    </div>
   );
 }
