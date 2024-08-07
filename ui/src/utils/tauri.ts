3f1e7a7da8156de68c04cf8212065987f7ff4f99
NextFire <git@yuru.moe>
2024-08-07 22:59:51 +0200

    ui: update tauri detection

--- ui/src/utils/tauri.ts
+++ ui/src/utils/tauri.ts
@@ -1,4 +1,5 @@
-export const isTauri = () => "__TAURI__" in window;
+export const isTauri = () =>
+  "__TAURI__" in window || "__TAURI_INTERNALS__" in window; // v1 || v2
 
 export const RELEASE_URL =
   "https://github.com/Japan7/karaberus/releases/latest";
