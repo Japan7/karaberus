86918e7e23d0fa959b0b523d901d8fec5592fec3
NextFire <git@yuru.moe>
2024-08-07 15:52:03 +0200

    desktop: fix mpv arg passing

--- ui/src/components/TauriKaraPlayer.tsx
+++ ui/src/components/TauriKaraPlayer.tsx
@@ -1,25 +1,35 @@
 import { invoke } from "@tauri-apps/api";
-import { Show } from "solid-js";
+import { HiSolidPlayCircle } from "solid-icons/hi";
+import { Show, type JSX } from "solid-js";
 import type { components } from "../utils/karaberus";
 import { getSessionToken } from "../utils/session";
 
 export default function TauriKaraPlayer(props: {
   kara: components["schemas"]["KaraInfoDB"];
 }) {
-  const getVideoSrc = () => `/api/kara/${props.kara.ID}/download/video`;
-  const getSubSrc = () => `/api/kara/${props.kara.ID}/download/sub`;
+  const downloadEndpoint = (type: string) =>
+    `${location.origin}/api/kara/${props.kara.ID}/download/${type}`;
+  const getVideoSrc = () =>
+    props.kara.VideoUploaded ? downloadEndpoint("video") : undefined;
+  const getInstSrc = () =>
+    props.kara.InstrumentalUploaded ? downloadEndpoint("inst") : undefined;
+  const getSubSrc = () =>
+    props.kara.SubtitlesUploaded ? downloadEndpoint("sub") : undefined;
 
-  const play = async () => {
+  const play: JSX.EventHandler<HTMLButtonElement, MouseEvent> = async (e) => {
+    e.preventDefault();
     await invoke("play", {
-      video: location.origin + getVideoSrc(),
-      sub: location.origin + getSubSrc(),
       auth: getSessionToken(),
+      video: getVideoSrc(),
+      inst: getInstSrc(),
+      sub: getSubSrc(),
     });
   };
 
   return (
-    <Show when={props.kara.VideoUploaded}>
-      <button onclick={play} class="btn btn-wide">
+    <Show when={props.kara.VideoUploaded || props.kara.InstrumentalUploaded}>
+      <button onclick={play} class="btn btn-lg btn-primary">
+        <HiSolidPlayCircle class="size-5" />
         Open in mpv
       </button>
     </Show>
