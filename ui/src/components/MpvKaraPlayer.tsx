1ea088526fc64981a16b537c57d40f5ec91f10b0
NextFire <git@yuru.moe>
2024-08-07 20:35:56 +0200

    desktop: mpv loading state

--- ui/src/components/MpvKaraPlayer.tsx
+++ ui/src/components/MpvKaraPlayer.tsx
@@ -8,6 +8,7 @@ export default function MpvKaraPlayer(props: {
   kara: components["schemas"]["KaraInfoDB"];
 }) {
   const [getLoading, setLoading] = createSignal(false);
+  const [getStarted, setStarted] = createSignal(false);
 
   const downloadEndpoint = (type: string) =>
     `${location.origin}/api/kara/${props.kara.ID}/download/${type}`;
@@ -34,17 +35,24 @@ export default function MpvKaraPlayer(props: {
     }
     const command = new Command("mpv", args);
     command.on("close", (exit) => {
-      console.debug(`command finished with code ${exit.code}`);
+      console.debug(`mpv exited with code ${exit.code}`);
       if (exit.code !== 0) {
         alert("An error occurred");
       }
       setLoading(false);
+      setStarted(false);
     });
     command.stdout.on("data", console.log);
+    command.stdout.on("data", (log: string) => {
+      if (log.startsWith("VO:")) {
+        setLoading(false);
+      }
+    });
     command.stderr.on("data", console.error);
     try {
       const handle = await command.spawn();
       console.debug(`mpv started with pid ${handle.pid}`);
+      setStarted(true);
     } catch (error) {
       console.error(error);
       alert("An error occurred");
@@ -55,12 +63,17 @@ export default function MpvKaraPlayer(props: {
   return (
     <Show when={props.kara.VideoUploaded || props.kara.InstrumentalUploaded}>
       <button
-        disabled={getLoading()}
+        disabled={getLoading() || getStarted()}
         onclick={play}
         class="btn btn-lg btn-primary"
       >
-        <HiSolidPlayCircle class="size-5" />
-        Open in mpv
+        <Show
+          when={!getLoading()}
+          fallback={<span class="loading loading-spinner loading-lg" />}
+        >
+          <HiSolidPlayCircle class="size-5" />
+          Open in mpv
+        </Show>
       </button>
     </Show>
   );
