f36f81f32aa37b81ffb37385e36807d9ae2bac3f
NextFire <git@yuru.moe>
2024-08-08 22:53:21 +0200

    ui: replace symlinks with vite-static-copy
    blame windows

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,12 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import pages from "vite-plugin-pages";
 import solid from "vite-plugin-solid";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig({
-  plugins: [solid(), pages()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:8888",
+export default defineConfig(({ mode }) => {
+  const plugins: PluginOption[] = [solid(), pages()];
+
+  if (mode.indexOf("tauri") !== 0) {
+    plugins.push(
+      viteStaticCopy({
+        targets: [
+          {
+            src: "node_modules/@fontsource/amaranth/files/*",
+            dest: "amaranth",
+          },
+          {
+            src: "node_modules/libass-wasm/dist/js/*",
+            dest: "libass-wasm",
+          },
+        ],
+      }),
+    );
+  }
+
+  return {
+    plugins,
+    server: {
+      proxy: {
+        "/api": "http://localhost:8888",
+      },
     },
-  },
+  };
 });
