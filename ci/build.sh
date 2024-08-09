b9748f192b2cf18b448d605b15819f69e6db6801
odrling <florianbadie@odrling.xyz>
2024-08-09 04:05:13 +0200

    ci: build libass from source
    not ideal because of the circular dep between harfbuzz and freetype but
    we don't really them yet.

--- ci/build.sh
+++ ci/build.sh
@@ -5,8 +5,6 @@ if [ -n "$CHOST" ]; then
     ln -sf ./libc.so /usr/${CHOST}/usr/lib/ld-musl-${ARCH}.so.1
 fi
 
-apk add libass-dev
-
 meson setup /build --buildtype release --strip -Dtest=false -Ds3_tests=disabled -Db_lto=true -Db_lto_mode=thin -Dffmpegaacsucks:default_library=static -Ddakara_check:default_library=static -Db_pie=true -Dffmpeg:programs=disabled -Dffmpeg:tests=disabled -Dffmpeg:encoders=disabled -Dffmpeg:muxers=disabled -Dffmpeg:avfilter=disabled -Dffmpeg:avdevice=disabled -Dffmpeg:postproc=disabled -Dffmpeg:swresample=disabled -Dffmpeg:swscale=disabled -Dffmpeg:decoders=disabled -Dffmpeg:aac_decoder=enabled -Dffmpeg:aac_fixed_decoder=enabled -Dffmpeg:aac_latm_decoder=enabled -Dffmpeg:version3=enabled $crossarg
 meson compile -C /build
 meson install -C /build --destdir /image --skip-subprojects
