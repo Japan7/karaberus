04f872ce1d63e045d5a7697f433307bd30f0e05c
odrling <florianbadie@odrling.xyz>
2024-08-12 16:45:49 +0200

    ci: install runtime tag
    should remove headers and other dev targets.
    strips about ~10MB from images

--- ci/build.sh
+++ ci/build.sh
@@ -4,5 +4,5 @@ if [ -n "${TARGET}" ]; then
 fi
 
 meson setup build --buildtype release --strip -Dtest=false -Ds3_tests=disabled -Db_lto=true -Db_lto_mode=thin -Db_pie=true -Dffmpeg:programs=disabled -Dffmpeg:tests=disabled -Dffmpeg:encoders=disabled -Dffmpeg:muxers=disabled -Dffmpeg:avfilter=disabled -Dffmpeg:avdevice=disabled -Dffmpeg:postproc=disabled -Dffmpeg:swresample=disabled -Dffmpeg:swscale=disabled -Dffmpeg:decoders=disabled -Dffmpeg:aac_decoder=enabled -Dffmpeg:aac_fixed_decoder=enabled -Dffmpeg:aac_latm_decoder=enabled -Dffmpeg:version3=enabled $crossarg
-meson install -C build --destdir image
+meson install -C build --destdir image --tags runtime
 ccache --show-stats
