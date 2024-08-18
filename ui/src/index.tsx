f819c392b46ef648642c24db1636c87f9c2393f3
NextFire <git@yuru.moe>
2024-08-18 23:59:26 +0200

    desktop: disable html overscroll behavior
    https://github.com/tauri-apps/tauri/issues/4309

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,8 +1,13 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
+import { isTauri } from "@tauri-apps/api/core";
 import App from "./App";
 import "./index.css";
+
+if (isTauri()) {
+  import("./index.tauri.css");
+}
 
 const root = document.getElementById("root");
 
