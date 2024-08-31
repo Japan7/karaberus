953a692e68544d92ec9d36bdc497bb0524e726e0
NextFire <git@yuru.moe>
2024-08-31 10:27:42 +0200

    server/ui: associate user timing profile

--- server/user.go
+++ server/user.go
@@ -37,3 +37,35 @@ func getOrCreateUser(ctx context.Context, sub string, info *oidc.UserInfo) (*Use
 	}
 	return &user, nil
 }
+
+type GetMeOutput struct {
+	Body User
+}
+
+func GetMe(ctx context.Context, input *struct{}) (*GetMeOutput, error) {
+	user := getCurrentUser(ctx)
+	return &GetMeOutput{Body: *user}, nil
+}
+
+type UpdateMeAuthorInput struct {
+	Body struct {
+		Id *uint `json:"id"`
+	}
+}
+
+type UpdateMeAuthorOutput struct {
+	Status int
+}
+
+func UpdateMeAuthor(ctx context.Context, input *UpdateMeAuthorInput) (*UpdateMeAuthorOutput, error) {
+	tx := GetDB(ctx)
+	user := getCurrentUser(ctx)
+	if input.Body.Id != nil {
+		if _, err := GetAuthorById(tx, *input.Body.Id); err != nil {
+			return nil, DBErrToHumaErr(err)
+		}
+	}
+	user.TimingProfileID = input.Body.Id
+	err := tx.Model(&user).Select("timing_profile_id").Updates(&user).Error
+	return &UpdateMeAuthorOutput{Status: 204}, DBErrToHumaErr(err)
+}
