0708ea160cda391aab01e402924321e107c53e69
odrling <florianbadie@odrling.xyz>
2024-09-07 23:38:39 +0200

    ci: build with -fhardened
    should be pretty low effort to support

--- ci/build.sh
+++ ci/build.sh
@@ -11,7 +11,7 @@ IMAGE=/karaberus/image
 mkdir -p ${IMAGE}/etc
 cp -r /etc/ssl ${IMAGE}/etc/
 
-meson setup /build /karaberus --buildtype release --strip --libdir lib --prefix ${IMAGE} -Dtest=false -Ds3_tests=disabled -Db_lto=true -Db_lto_mode=thin -Db_pie=true $crossarg
+meson setup /build /karaberus --buildtype release --strip --libdir lib --prefix ${IMAGE} -Dtest=false -Ds3_tests=disabled -Db_lto=true -Db_lto_mode=thin -Db_pie=true -Dc_args=-fhardened $crossarg
 meson install -C /build --tags runtime
 
 SYSROOT=${SYSROOT} /karaberus/ci/copyimglibs.py ${IMAGE} ${IMAGE}/bin/karaberus
