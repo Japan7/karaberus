32c7604d7ca7210cf1ec11fc7d6ca4d0452c3553
Pierre Jeanjean <rhiobet@gmail.com>
2024-11-12 21:17:40 +0100

    ui: fix main search

--- ui/src/utils/karaoke.ts
+++ ui/src/utils/karaoke.ts
@@ -9,32 +9,44 @@ export function karaFuseSearch(
   videoTagMap: TagMap<components["schemas"]["VideoTag"]>,
   search: string,
 ) {
+  const searchTerms: string[] = search.match(/("[^"]*"|[^\s]+)/g)!;
+
+  const fuseKeys = [
+    { name: "Artists.Name", weight: 2 },
+    { name: "Artists.AdditionalNames.Name" },
+    {
+      name: "audioTagsName",
+      getFn: (kara: components["schemas"]["KaraInfoDB"]) =>
+        kara.AudioTags?.map((tag) => audioTagMap.get(tag.ID)?.Name ?? "") ?? [],
+    },
+    { name: "Authors.Name", weight: 2 },
+    { name: "Comment" },
+    { name: "ExtraTitles.Name" },
+    { name: "Language" },
+    { name: "Medias.name", weight: 2 },
+    { name: "Medias.additional_name.Name" },
+    { name: "SourceMedia.name", weight: 2 },
+    { name: "Title", weight: 4 },
+    { name: "Version" },
+    {
+      name: "videoTagsName",
+      getFn: (kara: components["schemas"]["KaraInfoDB"]) =>
+        kara.VideoTags?.map((tag) => videoTagMap.get(tag.ID)?.Name ?? "") ?? [],
+    },
+  ];
+
   const fuse = new Fuse(karas, {
-    keys: [
-      { name: "Artists.Name", weight: 2 },
-      "Artists.AdditionalNames.Name",
-      {
-        name: "audioTagsName",
-        getFn: (kara) =>
-          kara.AudioTags?.map((tag) => audioTagMap.get(tag.ID)?.Name ?? "") ??
-          [],
-      },
-      { name: "Authors.Name", weight: 2 },
-      "Comment",
-      "ExtraTitles.Name",
-      "Language",
-      { name: "Medias.name", weight: 2 },
-      "Medias.additional_name.Name",
-      { name: "SourceMedia.name", weight: 2 },
-      { name: "Title", weight: 4 },
-      "Version",
-      {
-        name: "videoTagsName",
-        getFn: (kara) =>
-          kara.VideoTags?.map((tag) => videoTagMap.get(tag.ID)?.Name ?? "") ??
-          [],
-      },
-    ],
+    keys: fuseKeys,
+    threshold: 0.2, // The default threshold is way too high.
   });
-  return fuse.search(search);
+
+  // Fuse.js cannot match multiple keys on its own, so we need to iterate on
+  // them. See https://github.com/krisk/Fuse/issues/235.
+  return fuse.search({
+    $and: searchTerms.map((term: string) => ({
+      $or: fuseKeys.map((key: { name: string }) => ({
+        [key.name]: term,
+      })),
+    })),
+  });
 }
